c18544a728a72f68a0b1e6632287f9627978165c
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-09-13 16:16:12 +0530

    app working update

--- book-recommendation-app/src/App.js
+++ book-recommendation-app/src/App.js
@@ -1,9 +1,63 @@
+import React, { useState } from "react";
 import "./App.css";
 
+const bookDB = {
+  javascript: [
+    { name: "Eloquent JavaScript - Marijn Haverbeke", rating: "4/5" },
+    { name: "You Don't Know JS - Kyle Simpson", rating: "3.5/5" },
+  ],
+
+  fiction: [
+    {
+      name: "Da Vinci Code - Dan Brown",
+      rating: "5/5",
+    },
+    {
+      name: "Harry Potter Series - J.K. Rowling",
+      rating: "4.5/5",
+    },
+  ],
+
+  selfhelp: [
+    {
+      name: "Atomic Habits - James Clear",
+      rating: "4.5/5",
+    },
+    {
+      name: "The Alchemist - Paulo Coelho",
+      rating: "4/5",
+    },
+  ],
+};
+
 function App() {
+  const [selectedGenre, setGenre] = useState("selfhelp");
+
+  function genreClickHandler(genre) {
+    setGenre(genre);
+  }
+
   return (
     <div className="App">
-      <h1>bookipediaaaaa</h1>
+      <h1>📚 bookipediaaaaa</h1>
+      <p>Checkout my favorite books! Select a genre to get started</p>
+      <div>
+        {Object.keys(bookDB).map((genre) => (
+          <button onClick={() => genreClickHandler(genre)}>{genre}</button>
+        ))}
+      </div>
+      <hr />
+      <div>
+        <ul>
+          {bookDB[selectedGenre].map((book) => (
+            <li>
+              <div>{book.name}</div>
+              <br></br>
+              <div>{book.rating}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
