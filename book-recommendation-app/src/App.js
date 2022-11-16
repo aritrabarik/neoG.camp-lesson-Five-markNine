17ae4d375aa2660e38502c9748d43710ae63eb40
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-11-16 19:30:30 +0530

    added books to the existing list

--- book-recommendation-app/src/App.js
+++ book-recommendation-app/src/App.js
@@ -2,64 +2,84 @@ import React, { useState } from "react";
 import "./App.css";
 
 const bookDB = {
-  javascript: [
-    { name: "Eloquent JavaScript - Marijn Haverbeke", rating: "4/5" },
-    { name: "You Don't Know JS - Kyle Simpson", rating: "3.5/5" },
-  ],
+    javascript: [
+        {
+            name: "Eloquent JavaScript - Marijn Haverbeke",
+            rating: "4.2/5",
+        },
+        {
+            name: "You Don't Know JS - Kyle Simpson",
+            rating: "4.5/5",
+        },
+        {
+            name: "JavaScript The Good Parts - Douglas Crockford",
+            rating: "4/5",
+        },
+    ],
 
-  fiction: [
-    {
-      name: "Da Vinci Code - Dan Brown",
-      rating: "5/5",
-    },
-    {
-      name: "Harry Potter Series - J.K. Rowling",
-      rating: "4.5/5",
-    },
-  ],
+    fiction: [
+        {
+            name: "Da Vinci Code - Dan Brown",
+            rating: "5/5",
+        },
+        {
+            name: "Harry Potter Series - J.K. Rowling",
+            rating: "4.5/5",
+        },
+        {
+            name: "Animal Farm - George Orwell",
+            rating: "4/5",
+        },
+    ],
 
-  selfhelp: [
-    {
-      name: "Atomic Habits - James Clear",
-      rating: "4.5/5",
-    },
-    {
-      name: "The Alchemist - Paulo Coelho",
-      rating: "4/5",
-    },
-  ],
+    selfhelp: [
+        {
+            name: "Atomic Habits - James Clear",
+            rating: "4.5/5",
+        },
+        {
+            name: "The Alchemist - Paulo Coelho",
+            rating: "4/5",
+        },
+        {
+            name: "Ikigai: The Japanese Secret to a Long and Happy Life - Francesc Miralles and Hector Garcia",
+            rating: "4.2/5",
+        },
+    ],
 };
 
 function App() {
-  const [selectedGenre, setGenre] = useState("selfhelp");
+    const [selectedGenre, setGenre] = useState("selfhelp");
 
-  function genreClickHandler(genre) {
-    setGenre(genre);
-  }
+    function genreClickHandler(genre) {
+        setGenre(genre);
+    }
 
-  return (
-    <div className="App">
-      <h1>📚 bookipediaaaaa</h1>
-      <p>Checkout my favorite books! Select a genre to get started</p>
-      <div>
-        {Object.keys(bookDB).map((genre) => (
-          <button onClick={() => genreClickHandler(genre)}>{genre}</button>
-        ))}
-      </div>
-      <hr />
-      <div>
-        <ul>
-          {bookDB[selectedGenre].map((book) => (
-            <li>
-              <div>{book.name}</div>
-              <br></br>
-              <div>{book.rating}</div>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>📚 bookipediaaaaa</h1>
+            <p>Checkout my favorite books! Select a genre to get started</p>
+            <div>
+                {Object.keys(bookDB).map((genre) => (
+                    <button onClick={() => genreClickHandler(genre)}>
+                        {genre}
+                    </button>
+                ))}
+            </div>
+            <hr />
+            <div>
+                <ul>
+                    {bookDB[selectedGenre].map((book) => (
+                        <li>
+                            <div>{book.name}</div>
+                            <br></br>
+                            <div>{book.rating}</div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
